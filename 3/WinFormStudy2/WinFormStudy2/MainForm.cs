95300682e7af37c0160439390ec5cdcb3584b870
pid011 <pid011@kakao.com>
2020-09-21 10:35:29 +0900

    Add project

---
+++ 3/WinFormStudy2/WinFormStudy2/MainForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormStudy2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            MessageBox.Show($"{SystemInformation.PowerStatus.BatteryLifePercent}", "메시지박스", MessageBoxButtons.OKCancel);
+        }
+    }
+}
---
+++ 4/InputExample/InputExample/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InputExample
+{
+    public partial class Form1 : Form
+    {
+        private StringBuilder _inputStr = new StringBuilder();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetterOrDigit(e.KeyChar) || char.IsPunctuation(e.KeyChar) || char.IsSymbol(e.KeyChar))
+            {
+                _inputStr = _inputStr.Append(e.KeyChar);
+                Invalidate();
+            }
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawString(_inputStr.ToString() + "_", new Font("NanumGothic", 10), Brushes.Black, 10, 10);
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            _inputStr = e.KeyCode switch
+            {
+                Keys.Enter => _inputStr.AppendLine(),
+                Keys.Space => _inputStr.Append(" "),
+                Keys.Back => _inputStr.Length > 0 ? _inputStr.Remove(_inputStr.Length - 1, 1) : _inputStr,
+                _ => _inputStr
+            };
+
+            Invalidate();
+        }
+    }
+}
