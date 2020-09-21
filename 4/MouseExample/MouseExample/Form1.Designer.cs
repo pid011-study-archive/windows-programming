64b1157ecae4d0e31df4a394bc4d0a073803d9dd
pid011 <pid011@kakao.com>
2020-09-22 08:51:23 +0900

    Add week 4 study project

--- 4/MouseExample/MouseExample/Form1.Designer.cs
+++ 4/MouseExample/MouseExample/Form1.Designer.cs
@@ -28,10 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.OnMouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDown);
+            this.MouseEnter += new System.EventHandler(this.OnMouseEnter);
+            this.MouseLeave += new System.EventHandler(this.OnMouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnMouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnMouseUp);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- 4/MouseExample/MouseExample/Form1.cs
+++ 4/MouseExample/MouseExample/Form1.cs
@@ -1,20 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MouseExample
 {
     public partial class Form1 : Form
     {
+        private (int x, int y) _cursor = (0, 0);
+
         public Form1()
         {
             InitializeComponent();
+            // Cursor.Hide();
+        }
+
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Invalidate();
+            }
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Invalidate();
+            }
+        }
+
+        private void OnMouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Invalidate();
+            }
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                using (Graphics graphics = CreateGraphics())
+                {
+                    graphics.DrawLine(Pens.Black, _cursor.x, _cursor.y, e.X, e.Y);
+                    _cursor.x = e.X;
+                    _cursor.y = e.Y;
+                }
+                Invalidate();
+            }
+        }
+
+        private void OnMouseEnter(object sender, System.EventArgs e)
+        {
+            Text = "Mouse Enter";
+        }
+
+        private void OnMouseLeave(object sender, System.EventArgs e)
+        {
+            Text = "Mouse Leave";
         }
     }
 }
